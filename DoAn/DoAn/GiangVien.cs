ae601e4d631e5f1b63750e7dd267ed8b586efc38
VuHoangVu <148739682+vu2802@users.noreply.github.com>
2024-10-29 08:49:58 +0700

    Giang vien va sinh vien

---
+++ DoAn/DoAn/GiangVien.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAn
+{
+    internal class GiangVien : Nguoi
+    {
+        public string BoMon {  get; set; }
+        public List<string> DanhSachMonHocGiangDay { get; set; } = new List<string>();
+        public ThoiKhoaBieu LichDayGiangVien { get; set; } = new ThoiKhoaBieu();
+
+        public GiangVien(string hoTen, string maSo, string email, DateTime ngaySinh, string boMon) : base(hoTen, maSo, email, ngaySinh)
+        {
+            BoMon = boMon;
+        }
+
+        public void ThemMonHocGiangDay(string monHoc)
+        {
+            DanhSachMonHocGiangDay.Add(monHoc);
+            Console.WriteLine($"Giang vien {HoTen} da them mon giang day: {monHoc}");
+        }
+
+        public void GiangDayMonHoc(string monHoc)
+        {
+            if (DanhSachMonHocGiangDay.Contains(monHoc))
+            {
+                Console.WriteLine($"{HoTen} dang giang day mon hoc {monHoc}");
+            }
+            else
+            {
+                Console.WriteLine($"{HoTen} khong giang day mon hoc {monHoc}");
+            }
+        }
+
+        public void XemLichDay()
+        {
+            Console.WriteLine($"Lich day cua giang vien: {HoTen}");
+        }
+
+        public override void LayThongTin()
+        {
+            Console.WriteLine($"Giang vien: {HoTen}, Ma so: {MaSo}, Email: {Email}, Ngay sinh: {NgaySinh.ToShortDateString()}, Bo mon: {BoMon}");
+        }
+    }
+}
---
+++ DoAn/DoAn/ThoiKhoaBieu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAn
+{
+    internal class ThoiKhoaBieu
+    {
+        public List<string> MonHoc { get; set; } = new List<string>();
+        public Dictionary<string, string> LichHoc { get; set; } = new Dictionary<string, string>();
+
+        public void ThemMonHoc(string monHoc, string thoiGian)
+        {
+            MonHoc.Add(monHoc);
+            LichHoc[monHoc] = thoiGian;
+        }
+
+        public void XemThoiKhoaBieu()
+        {
+            Console.WriteLine("Thời khóa biểu:");
+            foreach (var monHoc in LichHoc)
+            {
+                Console.WriteLine($"- Môn: {monHoc.Key}, Thời gian: {monHoc.Value}");
+            }
+        }
+    }
+}
