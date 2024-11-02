a9ab01bdfc71e3fce3e92da62f5a23339b1c33ca
khoaTA1 <145079230+khoaTA1@users.noreply.github.com>
2024-11-02 20:05:06 +0700

    Update ThoiKhoaBieu.cs
    hoàn thiện các phương thức nhập, chỉnh sửa và in thời khóa biểu

--- upload/ThoiKhoaBieu.cs
+++ upload/ThoiKhoaBieu.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TKB
@@ -12,7 +13,7 @@ namespace TKB
     {
         private DateTime ngayBatDau;
         private DateTime ngayKetThuc;
-        private int tuanHoc;            // xem xet them truong so tuan hoc (vi du: tuan hoc so 1, so 2, ...)
+        private int tuanHoc;            
 
         List<HocPhan> danhSachHocPhan = new List<HocPhan>();           // class HocPhan can bo sung sau
 
@@ -52,7 +53,6 @@ namespace TKB
             get { return tuanHoc; }
             set { tuanHoc = value; }
         }
-
         public List<HocPhan> DanhSachHocPhan
         {
             get { return danhSachHocPhan; }
@@ -62,6 +62,12 @@ namespace TKB
         //--------
         // methods
         //--------
+        public void SaoLuu(ThoiKhoaBieu TKBKhac)
+        {
+            TKBKhac.ngayBatDau = this.ngayBatDau;
+            TKBKhac.ngayKetThuc = this.ngayKetThuc;
+            TKBKhac.DanhSachHocPhan = new List<HocPhan>(this.danhSachHocPhan);
+        }
         public void NhapDanhSachHocPhan()
         {
             // so luong mon hoc can nhap
@@ -83,29 +89,85 @@ namespace TKB
                 // them vao danh sach mon hoc
             }
         }
-        public void NhapTKBtuan1()
+        public void NhapTKB()
         {
             Console.Write("Nhap ngay bat dau tuan hoc (dd/mm/yyyy): ");
             ngayBatDau = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
 
             while (ngayBatDau.DayOfWeek != DayOfWeek.Monday)
             {
-                Console.Write($"Nhap lai ngay bat dau tuan hoc (nen bat dau tu thu hai, ngay vua nhap la {ngayBatDau.DayOfWeek}): ");
+                string Thu = "";
+                if (ngayBatDau.DayOfWeek == DayOfWeek.Tuesday) Thu = "Thu ba";
+                else if (ngayBatDau.DayOfWeek == DayOfWeek.Wednesday) Thu = "Thu tu";
+                else if (ngayBatDau.DayOfWeek == DayOfWeek.Thursday) Thu = "Thu nam";
+                else if (ngayBatDau.DayOfWeek == DayOfWeek.Friday) Thu = "Thu sau";
+                else if (ngayBatDau.DayOfWeek == DayOfWeek.Saturday) Thu = "Thu bay";
+                else if (ngayBatDau.DayOfWeek == DayOfWeek.Sunday) Thu = "Chu nhat";
+
+                Console.Write($"Nhap lai ngay bat dau tuan hoc (nen bat dau tu thu hai, ngay vua nhap la {Thu}): ");
                 ngayBatDau = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
             }
-            /*
-            Console.Write("Nhap ngay ket thuc tuan hoc (dd/mm/yyyy): ");
-            ngayKetThuc = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
-            */
             ngayKetThuc = ngayBatDau.AddDays(6);
 
-            tuanHoc = 1;
+            Console.Write("Nhap so thu tu cua tuan hoc: ");
+
+            while (true)
+            {
+                try
+                {
+                    tuanHoc = Convert.ToInt32(Console.ReadLine());
+
+                    if (TuanHoc <= 0) throw new Exception();
+                } catch (Exception)
+                {
+                    Console.Write("!! Nhap lai so thu tu tuan hoc: ");
+                    continue;
+                }
+
+                break;
+            }
 
             NhapDanhSachHocPhan();
         }
         public void ChinhSuaTKB()
         {
+            while (true)
+            {
+                // in danh sach mon hoc hien tai cung thong tin mon hoc co trong TKB
+                Console.Write("\nCac mon hoc hien co trong tuan hoc: ");
+                foreach (HocPhan HPDuyet in DanhSachHocPhan)
+                {
+                    Console.Write($"\n1> {HPDuyet.InThongTinDS()}.");
+                }
 
+                Console.Write("\nCac ki tu khac> Thoat chinh sua.");
+
+                // lua chon cua nguoi dung
+                Console.Write(">>> Ban can chinh sua mon hoc thu (nhap 1 hoac 2 hoac...): ");
+                string LuaChon = Console.ReadLine();
+
+                // phan loai
+                try
+                {
+                    Convert.ToInt32(LuaChon);
+                }
+                catch (Exception)
+                {
+                    return;
+                }
+
+                // neu la mon hoc can chinh sua
+                int i = Convert.ToInt32(LuaChon);
+
+                DanhSachHocPhan[i - 1].ChinhSuaHocPhan();
+
+                // tiep tuc hoac dung chinh sua
+                Console.Write(">>> Ban co can chinh sua tiep? (nhap 1 neu co, nhap cac ki tu con lai neu khong): ");
+                LuaChon = Console.ReadLine();
+
+                // dung vong lap while neu LuaChon khac "1"
+                if (string.Compare(LuaChon, "1", true) != 0) break;
+            }
         }
         public string TraVeThu(int x)
         {
@@ -117,10 +179,11 @@ namespace TKB
             else if (x == 6) return "thu bay";
             else return "chu nhat";
         }
-        public int InTKB(int DoRongOInput)
+        public int InTKB(int DoRongOInput, int HangInHPInput)
         {
+            Console.SetCursorPosition(0, HangInHPInput);
             // In 2 dong dau cua TKB
-            Console.WriteLine($"Tuan hoc: 1\nNgay bat dau - ket thuc: {ngayBatDau.ToString("dd/MM/yyyy")} - {ngayKetThuc.ToString("dd/MM/yyyy")}");
+            Console.WriteLine($"Tuan hoc: {tuanHoc}\nNgay bat dau - ket thuc: {ngayBatDau.ToString("dd/MM/yyyy")} - {ngayKetThuc.ToString("dd/MM/yyyy")}");
 
             // do rong o la be ngang cua o, do rong nua o la 1/2 be ngang cua o (lam tron xuong)
             // so 7 la do dai lay chung cho cac chuoi in thu: "thu hai"
@@ -128,7 +191,7 @@ namespace TKB
             int DoRongNuaO = (DoRongO - 7) / 2;
 
             // in ki tu "|" dau tien
-            Console.SetCursorPosition(DoRongO, 3);
+            Console.SetCursorPosition(DoRongO, HangInHPInput + 3);
             Console.Write("|");
 
             // in thu ngay trong tuan kem cac ki tu "|" ngan cach
@@ -136,11 +199,11 @@ namespace TKB
             for (int i = 1; i <= 7; i++)
             {
                 // dat con tr chuot console de in cac thu ngay
-                Console.SetCursorPosition(i * DoRongO + DoRongNuaO, 3);
+                Console.SetCursorPosition(i * DoRongO + DoRongNuaO, HangInHPInput + 3);
                 Console.Write($"{this.TraVeThu(i)}");
 
                 // in ki tu "|" ngan cach
-                Console.SetCursorPosition((i + 1) * DoRongO, 3);
+                Console.SetCursorPosition((i + 1) * DoRongO, HangInHPInput + 3);
                 Console.Write("|");
             }
             // in hang "-", so 8 la 7 cot thu ngay + cot dau tien, hien tai la hang so 4
@@ -149,7 +212,7 @@ namespace TKB
             // In lich hoc theo tung tiet trong ngay, bat dau tu tiet 1
             List<HocPhan> DanhSachHPTam = danhSachHocPhan;
 
-            int HangInHP = 5;
+            int HangInHP = HangInHPInput + 5;
             int CotInHP = 0;
             int ChieuCaoMax = 0;
 
@@ -198,7 +261,7 @@ namespace TKB
             // in ki tu "|" ngan cach cac o cot thu ngay 
             for (int i = 1; i <= 8; i++)
             {
-                for (int j = 5; j <= HangInHP - 1; j++)
+                for (int j = HangInHPInput + 4; j <= HangInHP - 1; j++)
                 {
                     Console.SetCursorPosition(i * DoRongO, j);
                     Console.Write("|");
@@ -208,5 +271,6 @@ namespace TKB
             // tra ve vi tri hang o cuoi thoi khoa bieu
             return HangInHP;
         }
+        public void KiemTraTrung() { }
     }
 }
