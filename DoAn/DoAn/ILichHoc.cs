275836eb44ae1a281304b35a7232817433a3388b
VuHoangVu <148739682+vu2802@users.noreply.github.com>
2024-11-05 08:33:57 +0700

    Them interface va nhap thong tin nguoi dung

---
+++ DoAn/DoAn/ILichHoc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAn
+{
+    internal interface ILichHoc
+    {
+        void ThemMonHoc(string monHoc, string thoiGian);
+        void XemThoiKhoaBieu();
+    }
+}
