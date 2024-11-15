6b743b4745ad36917a5fec867677e9c5584d9ffa
khoaTA1 <145079230+khoaTA1@users.noreply.github.com>
2024-11-15 17:35:34 +0700

    Update Program.cs
    loại bỏ set buffer cho console (để mặc định)

--- upload/Program.cs
+++ upload/Program.cs
@@ -10,19 +10,20 @@ namespace TKB
     {
         static void Main(string[] args)
         {
-            Console.SetBufferSize(200, 140);
-            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
-            //Console.SetWindowSize(200, Console.LargestWindowHeight);
-
+            // Console.SetBufferSize(Console.LargestWindowWidth + 100, Console.LargestWindowHeight + 100);
+            int BeNgangConsole = Console.LargestWindowWidth - 5;
+            int ChieuDocConsole = Console.LargestWindowHeight;
+            int BeNgangOTKB = BeNgangConsole / 8 - 1;
+            Console.SetWindowSize(BeNgangConsole, ChieuDocConsole);
 
             HocPhan hp1 = new HocPhan("HP 1", "111", "werf rgf ytre wertghre vghj", "A1", 3, 1, 0);
             HocPhan hp2 = new HocPhan("HP 2", "112", "Giang vien B", "B2", 2, 5, 0);
             HocPhan hp3 = new HocPhan("HP bdfhg rfhj rfhg 234 432", "113", "Giang vien C", "A2", 4, 1, 3);
             HocPhan hp4 = new HocPhan("HP 4", "114", "Giang vien D", "C4", 2, 3, 5);
             HocPhan hp5 = new HocPhan("HP 5", "115", "Giang vien E", "A1", 3, 4, 5);
-            HocPhan hp6 = new HocPhan();
+            // HocPhan hp6 = new HocPhan();
 
-            hp6.NhapHocPhan();
+            // hp6.NhapHocPhan();
 
             List<HocPhan> DS_HP = new List<HocPhan>() {hp1, hp2, hp3, hp4, hp5};
 
@@ -33,32 +34,18 @@ namespace TKB
             TKB.NgayKetThuc = TKB.NgayBatDau.AddDays(6);
             TKB.TuanHoc = 1;
 
+            // TKB2.NhapTKB();
+
             QuanLyTKB QLTKB = new QuanLyTKB();
 
-            /*
             QLTKB.NhanBanTuanHoc(TKB);
 
-            QLTKB.LamTrongTKB();
-
-            int VTChuot = QLTKB.InTKBTheoTuan(20);
-            // TKB.KiemTraTrung();
-            Console.SetCursorPosition(0, VTChuot);
-
-            QLTKB.HuyHocPhan();
-
-            QLTKB.ChinhSuaTuanHoc();
-
-            VTChuot = QLTKB.InTKBTheoTuan(20);
-            */
-
-            QLTKB.NhanBanTuanHoc(TKB);
-
-            int VTChuot = QLTKB.InTKBTheoTuan(20);
+            int VTChuot = QLTKB.InTKBTheoTuan(BeNgangOTKB);
             Console.SetCursorPosition(0, VTChuot);
 
             QLTKB.ChinhSuaTuanHoc();
 
-            VTChuot = QLTKB.InTKBTheoTuan(20);
+            VTChuot = QLTKB.InTKBTheoTuan(BeNgangOTKB);
             // TKB.KiemTraTrung();
             Console.SetCursorPosition(0, VTChuot);
 
